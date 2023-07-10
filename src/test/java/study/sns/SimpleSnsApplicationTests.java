3cc3c49c6b077658cb8afc74e7be2c3fdcd59234
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-07-10 17:19:47 +0900

    init: 프로젝트 개발 환경 구성

---
+++ src/test/java/study/sns/SimpleSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package study.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
